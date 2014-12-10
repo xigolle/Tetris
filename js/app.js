c89cce4f3e57db3b294f68776b175853f58f93ca
xigolle <joey.driessen@live.nl>
2014-12-10 12:34:39 +0100

    DrawPiece functie
    Functie gemaakt voor het tekenen van een tetris blok. Echter zit nog een
    fout in de T piece

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ var main = function () {
 	var canvas = document.getElementById("playBoard");
 	//context zetten naar 2d
 	var ctx = canvas.getContext("2d");
-	//test object voor code voor de tetris pieces
-	var piece = new lPiece();
 	//size van de grid blokken 
 	var size = 32;
+	var timer;
 	var bgImg = new Image();
+	var currentPiece = 
 	bgImg.onload = function(){
 		ctx.drawImage(bgImg, 0, 0, 320, 640);
 		//start de game pas wanneer de image geladen is
-		init();
+		drawPiece(randomPiece());
 	}
 	bgImg.src="images/bg.png";
 
@@ -20,31 +20,28 @@ var main = function () {
 
 
 
-	//deze functie werkt met harde coded blokken. Functie moet nog aangepast worden zodat elke blok als parameter kan worden meegegeven.
-	//Tevens nakijken hoe hij het best in het midden kan beginnen en misschien alvast nadenken over het zakken van de blokken.
-	function init(){
-		for(var i = 0; i < piece.pos3.length;i++){
-			var segment = piece.pos3[i];
-			piece.gridx = 0;
-			
-			for(var j = 0 ; j < segment.length; j++)
-			{	
-				if (piece.pos3[i][j]=== 1 ) {
-					console.log(1);
-					ctx.fillStyle = "red";
-					ctx.fillRect(piece.gridx,piece.gridy,size,size);
-					piece.gridx += size;
-
+	//functie tekent verschillende blokken zitten echter wel een paar fouten bij. Bijvoorbeeld de T piece
+	function drawPiece(p){
+		console.log(p);
+		for (var i = 0; i<p.pos1.length;i++){
+			var segment = p.pos1[i];
+			p.gridx = 0;
+			for(var j = 0; j<segment.length; j++){
+				if(p.pos1[i][j] ===1){
+				ctx.fillStyle = "red";
+				ctx.fillRect(p.gridx,p.gridy,size,size);
+				p.gridx+=size;
 				}
-				else if(piece.pos3[i][j]=== 0)
-				{
-					piece.gridx += size;
-					console.log("0");
+				else if(p.pos1[i][j]){
+					p.gridx += size;
 				}
-
 			}
-			piece.gridy += size;
+			p.gridy += size;
 		}
+		
+	}
+	function init(){
+		
 	}
 	
 	
